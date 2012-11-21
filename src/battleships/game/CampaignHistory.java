f84bcd3b514dd5ddbf46b2fe7c19dbe6a0da4c85
Edgar Streuli <edgarstreuli@gmail.com>
2012-11-21 13:57:30 -0300

    guardando datos

---
+++ src/battleships/game/CampaignHistory.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleships.game;
+
+/**
+ *
+ * @author edster
+ */
+public class CampaignHistory {
+    
+    public static final String[]HISTORY = {
+        "Habia una vez un gato montez",
+        "El enemigo ha llegado a las puertas",
+        "LA batalla se ha extendido por el mundo!!!",
+        "Ya no quedan mas barcos",
+        "Sólo queda el torpedo"
+    };
+    
+}
